d30d5c18704b1317f75b90fb3a32be140329b9d5
Abhinandan Chakraborty <abchaks99@gmail.com>
2022-07-22 08:08:38 +0530

    init

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user=User.find_by(email: params[:email])
+        @user=User.find_by(email: params[:email].downcase)
 
         if @user && @user.authenticate(params[:password])
             session[:user_id]=@user.id
